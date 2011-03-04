786947a4b5ba8bc6df03cf25a404ef6078eb2be8
jeresig <jeresig@gmail.com>
2011-03-04 01:42:38 -0500

    Add a test web server (written in Node.js) for serving up the index page - dictionary doesn't load, yet.

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var fs = require("fs"),
+	http = require("http"),
+	
+	// Should switch to 80 when ready-to-go
+	port = 8080,
+	
+	// Hosted on another dummy server, for now
+	cdn = "//localhost:8888/deepleap/";
+	
+fs.readFile( "index.html", "utf8", function( err, data ) {
+	data = data.replace( /(src|href)="\/?(?!http)/g, '$1="' + cdn );
+	
+	http.createServer(function( request, response ) {
+		response.writeHead( 200, { "Content-type": "text/html" } );
+		response.end( data );
+		
+		// TODO: Write logic for handling score submissions, score checking, etc.
+	}).listen( port );
+});
