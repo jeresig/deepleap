1cbc1954f65e5e8a3d490035816accba3ae24e29
jeresig <jeresig@gmail.com>
2011-03-07 19:03:53 -0500

    Add in logic for saving a replay to the database.

--- config/db.sql
+++ config/db.sql
@@ -1,19 +1,19 @@
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL DEFAULT '',
   `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
   `password` char(40) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  PRIMARY KEY (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `replays` (
   `hash` char(40) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `seed` int(11) DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
+  `verified` bit(1) DEFAULT NULL,
+  `start` varchar(15) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `log` text,
   PRIMARY KEY (`hash`)
