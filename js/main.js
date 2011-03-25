92d0b1552f50e95a9609480763696955351f3e8a
jeresig <jeresig@gmail.com>
2011-03-25 17:48:12 -0400

    Adding a main loading screen at the start.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 jQuery(function() {
+	jQuery( "#overview-all" ).dialog({
+		title: "DeepLeap",
+		modal: true,
+		draggable: false,
+		resizable: false,
+		width: 480
+	}).parent().find(".ui-dialog-titlebar-close").remove();
+	
+	jQuery( "#play-random" ).click(function() {
+		jQuery( "#overview-all" ).dialog( "close" );
+		
+		startGame();
+		
+		return false;
+	});
+	
 	// Get the seed from the query string (if it exists)
 	// and set the seed on the game
 	Game.setSeed( parseInt( (/game=(\d+)/.exec( location.search ) || [0,0])[1] ) );
@@ -10,7 +26,7 @@ jQuery(function() {
 	// Load in the dictionary from the server
 	} else {
 		jQuery.ajax({
-			url: cdnHREF + "dict/dict.js",
+			url: cdnHREF + "dict/ptrie.js",
 			dataType: "jsonp",
 			jsonp: false,
 			jsonpCallback: "dictLoaded",
@@ -20,7 +36,7 @@ jQuery(function() {
 					window.localStorage.gameDict = txt;
 				}
 
-				dictReady( txt )
+				dictReady( txt );
 			}
 			// TODO: Add error/timeout handling
 		});
@@ -30,7 +46,24 @@ jQuery(function() {
 function dictReady( txt ) {
 	// Pass the dictionary into the game
 	Game.loadDict( txt );
+	
+	// Create a sample game that will be played in the main menu
+	jQuery( "#sample-game" )
+		// The mini game is much smaller
+		.game({ tileMargin: 3, tileWidth: 20, tileTopMargin: 3 })
+		
+		// Seed it with a sample game
+		.game("playback", [0,2744,[1,0],928,1247,2408,[4,0],1401,847,3665,[3,0],1152,[3,1],1152,[4,3],2480,[8,4],1400,[6,5],792,6416,[4,0],1312,[4,3],1368,[7,4],1560,[8,4],920,6056,[4,0],1584,[5,1],1272,[3,2],1584,[6,3],1480,[7,4],2304,[5,3],1096,[5,4],912,5216,[4,0],1448,[6,1],2776,[7,4],824,11440,[3,0],1336,[6,1],2736,[5,2],832,7120,[7,0],1680,[6,1],1416,[4,2],4344,11529,[6,1],1832,[8,2],2072,[6,3],1304,4200,[8,1],2231,[6,2],1449,4071,[1,0],1729,[4,1],1168,[4,2],4024,[8,3],1176,14832,[4,0],800,[2,1],1664,3679,[3,0],1393,[3,1],2984,1784,[2,0],976,[2,1],760,[3,2],776,[4,3],840,11336,[1,0],1072,[3,1],1417,[7,2],799,8928,[2,0],1745,[5,1],1447,[3,2],1760,[7,2],1329,10345,8267,6224,4148,2066,167])
+		
+		// Reveal the mini game
+		.show();
+}
 
+function startGame() {
+	if ( !Game.dict ) {
+		return;
+	}
+	
 	// Need to start the game
 	jQuery( "#main" ).game().game("start");
 	
