cb63201cc6c3d7663ecf96c3579190c361112bcd
John Resig <jeresig@gmail.com>
2015-02-01 21:16:26 -0500

    Work-in-progress user account handling.

--- www/js/user.js
+++ www/js/user.js
@@ -1,15 +1,63 @@
 var User = Backbone.Model.extend({
-    
-});
+    initialize: function(options) {
+        this.auth = options.auth;
+    },
 
-User.autoAuth = function() {
-    document.addEventListener("deviceready", function() {
-        if (typeof gamecenter !== "undefined") {
-            gamecenter.auth(function(user) {
-                gameUI.setUser(user);
-            }, function() {
-                // Failure.
-            });
-        }
-    }, false);
-};
+    updateFromData: function(data) {
+        // TODO: Update user object
+        // TODO: Update localforage record
+    },
+
+    syncWithServer: function() {
+        var self = this;
+
+        // /auth/gamecenter
+        $.ajax({
+            type: "POST",
+            url: self.server + "/auth/gamecenter",
+            contentType: "application/json",
+            data: JSON.stringify(this.auth),
+            dataType: "json",
+            success: function(results) {
+                // Load updated user data
+                user.updateFromData(results.user);
+            }
+        });
+    }
+}, {
+    setCurrentUser: function(user) {
+        // TODO: Notify gameUI
+        this.currentUser = user;
+    },
+
+    getCurrentUser: function() {
+        return this.currentUser;
+    },
+
+    createUserFromAuth: function(auth) {
+        
+    },
+
+    autoAuth: function() {
+        localforage.getItem("snp-user", function(err, userData) {
+            if (userData) {
+                User.setCurrentUser(new User(userData)));
+            }
+
+            document.addEventListener("deviceready", function() {
+                if (typeof gamecenter !== "undefined") {
+                    gamecenter.auth(function(auth) {
+                        var curUser = User.getCurrentUser();
+
+                        if (curUser && !curUser.verifyAuth(auth) || !curUser) {
+                            var user = User.createUserFromAuth(auth);
+                            User.setCurrentUser(user);
+                        }
+                    }, function() {
+                        // Failure.
+                    });
+                }
+            }, false);
+        });
+    }
+});
